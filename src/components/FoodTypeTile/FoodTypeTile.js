e67cb7d8ed4b810e8de78a5c5800d549400de62c
unknown <dinuka9131@gmail.com>
2023-03-22 19:50:07 +0530

    Complete breakfast

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -6,8 +6,10 @@ const FoodTypeTile = () => {
   return (
     <div className={classes.outer}>
         <img  className={classes.image}src={breakfast_image}/>
-        <span className={classes.mainTitle}>Breakfast</span>
-        <span className={classes.subTitle}>13 Items</span>
+        <div className={classes.titleHolder}>
+            <span className={classes.mainTitle}>Creakfast</span>
+            <span className={classes.subTitle}>13 Items</span>
+        </div>
     </div>
   )
 }
