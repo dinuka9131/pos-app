ab025e073e7ad9efc62a5244bef29d3906efd106
unknown <dinuka9131@gmail.com>
2023-03-22 21:06:05 +0530

    change color

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import classes from './FoodTypeTile.module.css'
 import breakfast_image from '../../Assets/breakfast.png'
 
 const FoodTypeTile = () => {
+
+  const [mouseState, setMouseEntered] = useState(false)
+
+  const mouseEnterHandler = () => {
+    setMouseEntered(true)
+  }
+
+  const mouseLeaveHandler = () => {
+    setMouseEntered(false)
+  }
+
   return (
-    <div className={classes.outer}>
+    <div 
+      className={classes.outer} 
+      onMouseEnter={mouseEnterHandler}
+      onMouseLeave={mouseLeaveHandler}
+      style={{backgroundColor: !mouseState? 'yellow':'orange'}}
+    >
         <img  className={classes.image}src={breakfast_image}/>
         <div className={classes.titleHolder}>
-            <span className={classes.mainTitle}>Creakfast</span>
+            <span className={classes.mainTitle}>Breakfast</span>
             <span className={classes.subTitle}>13 Items</span>
         </div>
     </div>
