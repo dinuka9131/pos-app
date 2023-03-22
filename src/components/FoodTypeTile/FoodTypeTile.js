f002a472465c6f995b68975a675c348b76d2abf6
unknown <dinuka9131@gmail.com>
2023-03-22 15:06:18 +0530

    Image Added

---
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './FoodTypeTile.module.css'
+import breakfast_image from '../../Assets/breakfast.png'
+
+const FoodTypeTile = () => {
+  return (
+    <div>
+        <img src={breakfast_image}/>
+    </div>
+  )
+}
+
+export default FoodTypeTile
